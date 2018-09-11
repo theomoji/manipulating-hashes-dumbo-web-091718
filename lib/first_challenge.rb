3281362ad7e6d26d3e72f057f8df6ea869e3e115
Shuo Wang <gken90@gmail.com>
2018-09-11 18:18:46 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,7 +12,8 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-contacts.each do |attribute, value|
+
+data.each do |attribute, value|
   if attribute == :favorite_icecream_flavors
       value.delete_if {|flavor| flavor == "strawberry"}
   end
