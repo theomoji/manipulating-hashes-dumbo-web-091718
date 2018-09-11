8e4fc7316b296fc07e619e77444d215b9c4f198f
Shuo Wang <gken90@gmail.com>
2018-09-11 18:20:01 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,7 @@ def first_challenge
     }
   }
 
-data.each do |attribute, value|
+contact.each do |attribute, value|
   if attribute == :favorite_icecream_flavors
       value.delete_if {|flavor| flavor == "strawberry"}
   end
